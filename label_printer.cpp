6ecf6d4f673ddbff62fb262f0f6ea89c3636f2c5
shoenfelte <shoenfelte@vt.edu>
2026-09-16 21:55:59 -0400

    Add files via upload

---
+++ label_printer.cpp
@@ -0,0 +1,8 @@
+#include "label_printer.h"
+#include <QDebug>
+
+LabelPrinter::LabelPrinter(const char * labelstr): QLabel(labelstr) {};
+
+void LabelPrinter::keyPressEvent(QKeyEvent *ev){
+    qDebug() << "Hello World!";
+}
---
+++ label_printer.h
@@ -0,0 +1,14 @@
+#ifndef LABEL_PRINTER_H
+#define LABEL_PRINTER_H
+
+#include <QLabel>
+
+class LabelPrinter: public QLabel
+{
+    Q_OBJECT
+public:
+    LabelPrinter(const char * labelstr);
+    void keyPressEvent(QKeyEvent *ev);
+};
+
+#endif
